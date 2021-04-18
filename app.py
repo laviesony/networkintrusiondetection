7d54ae274e19bb4ace871d2edc8eb1758d7b6e61
Mahip Soni <mahip.mailbox@gmail.com>
2021-04-18 16:01:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Created on April  16 2021
-
-@author: pattn
-"""
-
-
-# coding: utf-8
-
+import imghdr
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from flask import Flask, request, render_template
 import pickle
 import xgboost as xgb
-#import csv
+import time
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS'] = ['.csv']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 
 
-app = Flask("__name__")
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST'])
+def upload_files():
+	uploaded_file = request.files['file']
+	filename = secure_filename(uploaded_file.filename)
+	if filename != '':
+		file_ext = os.path.splitext(filename)[1]
+	if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+		abort(400)
+
+	uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+	t0=time.time()
+	print("yes")
+	model = pickle.load(open("xgb_model.pkl", "rb"))    
+	new_df = pd.read_csv('uploads/'+filename)
+
+	y_pred = model.predict(new_df)
+
+	o1 = "The list contains all the predicted outputs"
+	o2 = ""
+	for i in y_pred:
+		o2 = o2+str(i)+"\n"
+
+	t1=time.time()
+	o3=t1-t0
+	return render_template('home.html', output1=o1, output2=o2,output3="Time elpased = "+str(o3))
+ 
+
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+	return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 
 
-q = ""
 
-@app.route("/")
-def loadPage():
-	return render_template('home.html', query="")
-
-
-@app.route("/predict", methods=['POST'])
-def predict():
-    
-    inputQuery1 = request.form['query1']
-    model = pickle.load(open("xgb_model.pkl", "rb"))
-    
-    
-    new_df = pd.read_csv("nid.csv")
-    
-    y_pred = model.predict(new_df)
-    #probablity = model.predict_proba(new_df)[:,1]
-    
-    o1 = "The list contains all the predicted outputs"
-    o2 = ""
-    for i in y_pred:
-        o2 = o2+str(i)+"\n"
-    
-#     if single==1:
-#         o1 = "The patient is diagnosed with Breast Cancer"
-#         o2 = "Confidence: {}".format(probablity*100)
-#     else:
-#         o1 = "The patient is not diagnosed with Breast Cancer"
-#         o2 = "Confidence: {}".format(probablity*100)
-        
-    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'])
-    
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+   app.run(debug = True)
 
